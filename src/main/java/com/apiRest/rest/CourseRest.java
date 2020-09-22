ef3aa639ab71eef8b2b1b9e7b3dc824a89121982
Joaquín Dreau <joaquindreau@gmail.com>
2020-09-22 19:01:04 -0300

    avance de metodo post student xxxx metodo post course listo

--- src/main/java/com/apiRest/rest/CourseRest.java
+++ src/main/java/com/apiRest/rest/CourseRest.java
@@ -2,20 +2,22 @@ package com.apiRest.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.apiRest.dao.CourseDAO;
+import com.apiRest.model.course;
 
 @RestController
-@RequestMapping("cursos")
+@RequestMapping("course")
 public class CourseRest {
 	
 	@Autowired
 	private CourseDAO courseDAO;
 	
-//	@PostMapping("/guardar")
-//	public void guardar() {
-//		courseDAO.save();
-//	}
+	@PostMapping("/guardar")
+	public void guardar(@RequestBody course course) {
+		courseDAO.save(course);
+	}
 }
--- src/main/java/com/apiRest/rest/studentRest.java
+++ src/main/java/com/apiRest/rest/studentRest.java
@@ -1,8 +1,22 @@
 package com.apiRest.rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class studentRest {
+import com.apiRest.dao.StudentDAO;
+import com.apiRest.model.student;
 
+@RestController
+@RequestMapping("student")
+public class studentRest {
+	@Autowired
+	private StudentDAO studentDAO;
+	
+	@PostMapping("/guardar")
+	private void guardar(@RequestBody student student) {
+		studentDAO.save(student);
+	}
 }
